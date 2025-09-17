b3a3e7221bc9b09f6e4665871cf7f4092e68a871
fran <franlopezpineda@gmail.com>
2025-09-17 12:36:31 +0200

    resolvemos ejerc 3 y 4

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function pintar_ruta(){
-    console.log("El acesso a la ruta C:\\\\usuario\\tarda 1'23\", algo considerado \"lento\" en la actualidad. ");
-}
-
 function saludar(){
     var edad = window.prompt("Introduce tu edad")
        if(edad >= 18){
@@ -9,4 +5,23 @@ function saludar(){
         }else{
         console.log("Eres menor de edad");
         }
+}
+
+function pintar_ruta(){
+    console.log("El acesso a la ruta C:\\\\usuario\\tarda 1'23\", algo considerado \"lento\" en la actualidad. ");
+}
+
+function pintar_ruta_v2(){
+    var substrng1 = "El acesso a la ruta C:\\\\usuario\\tarda 1'23\",algo";
+    var substrng2 = " considerado \"lento\" en la actualidad ";
+    console.log(substrng1 + substrng2);
+
+}
+
+
+
+// Ejercicio 4
+function muestra_potencia(){
+    var num = 2*Math.pow(10,-9);
+    console.log("El valor es " + num);
 }
